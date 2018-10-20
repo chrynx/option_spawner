03c3127070ac935f07669b6c183d6bb87bfdc8e5
Ralph Wendt <ralphmadriaga@gmail.com>
2018-10-20 13:05:05 +0100

    added a to and from select, also added state variables for manipulation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      fromMonth: "",
+      toMonth: "",
+      fromYear: 0,
+      toYear: 0,
       months: ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"],
       readableMonths: ["January","February","March","April","May","June","July","August","September","October","November","December"],
     };
@@ -12,24 +16,36 @@ class App extends Component {
   }
 
   componentDidMount() {
+      const date = new Date();
       const please = {
           render: {
               options: {
+                  /**
+                   *
+                   * @param fromMonth str The starting month of the options format = "JAN","FEB"...
+                   * @param fromYear num The starting year of the options
+                   * @returns {{to: (function(*=, *): {using: {select: using.select}})}}
+                   */
                   from: (fromMonth, fromYear) => {
                       return {
-                          to: (toMonth, toYear) => {
+                          /**
+                           *
+                           * @param toMonth str End month of the options, default is current month format = "JAN", "FEB"...
+                           * @param toYear num The end year of the options, default is current year
+                           * @returns {{using: {select: using.select}}}
+                           */
+                          to: (toMonth = this.state.months[date.getMonth()], toYear = date.getFullYear()) => {
                               return {
                                   using: {
+                                      /**
+                                       *
+                                       * @param select str The ID of the select that you are using
+                                       */
                                       select: (select) => {
                                           const startY = fromYear;
                                           const endY = toYear;
                                           const startM = this.state.months.indexOf(fromMonth);
                                           let endM = this.state.months.indexOf(toMonth);
-
-                                          console.log("startY :", startY);
-                                          console.log("endY :", endY);
-                                          console.log("startM :", startM);
-                                          console.log("endM :", endM);
 
                                           for(let i = startY; i <= endY; i++) {
                                             for(let j = startM; j <= (i < endY ? 11 : endM); j++) {
@@ -47,18 +63,57 @@ class App extends Component {
                       }
                   }
               }
+          },
+          /**
+           *
+           * @param item any Log the item you wish to log
+           */
+          log: (item) => {
+            console.log(item);
           }
       };
-      const date = new Date();
-      please.render.options.from("JAN",1970).to(this.state.months[date.getMonth()],date.getFullYear()).using.select("dateSelect");
+
+      please.render.options.from("JAN",1970).to().using.select("dateSelect");
+      please.render.options.from("JAN",1970).to().using.select("toSelect");
+      document.querySelector("#dateSelect").addEventListener("change", (e) => {
+        const fromValues = e.target.value.split("|");
+        this.setState({
+            fromMonth: fromValues[0],
+            fromYear: parseInt(fromValues[1]),
+        });
+          please.log(this.state.fromMonth);
+          please.log(this.state.fromYear);
+      });
+      document.querySelector("#toSelect").addEventListener("change", (e) => {
+          const toValues = e.target.value.split("|");
+          this.setState({
+              toMonth: toValues[0],
+              toYear: parseInt(toValues[1]),
+          });
+          please.log(this.state.toMonth);
+          please.log(this.state.toYear);
+      });
+      please.log(this.state.fromMonth);
+      please.log(this.state.toMonth);
+      please.log(this.state.fromYear);
+      please.log(this.state.toYear);
   }
 
   render() {
     return (
       <div className="App">
-          <select name="dateSelect" id="dateSelect">
-              <option disabled readOnly value="none" selected="selected">Select a Date</option>
-          </select>
+          <div className="selectBox">
+              <label htmlFor="dateSelect">From: </label>
+              <select name="dateSelect" id="dateSelect">
+                  <option disabled readOnly value="none" selected="selected">Select a Date</option>
+              </select>
+          </div>
+        <div className="selectBox">
+            <label htmlFor="toSelect">To: </label>
+            <select name="toSelect" id="toSelect">
+                <option disabled readOnly value="none" selected="selected">Select a Date</option>
+            </select>
+        </div>
       </div>
     );
   }
