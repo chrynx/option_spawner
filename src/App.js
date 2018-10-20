9f5148786d05a332eed2563515bdc6d34579734c
Ralph Wendt <ralphmadriaga@gmail.com>
2018-10-20 12:24:20 +0100

    added the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      months: ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"],
+      readableMonths: ["January","February","March","April","May","June","July","August","September","October","November","December"],
+    };
+
+  }
+
+  componentDidMount() {
+      const please = {
+          render: {
+              options: {
+                  from: (fromMonth, fromYear) => {
+                      return {
+                          to: (toMonth, toYear) => {
+                              return {
+                                  using: {
+                                      select: (select) => {
+                                          const startY = fromYear;
+                                          const endY = toYear;
+                                          const startM = this.state.months.indexOf(fromMonth);
+                                          let endM = this.state.months.indexOf(toMonth);
+
+                                          console.log("startY :", startY);
+                                          console.log("endY :", endY);
+                                          console.log("startM :", startM);
+                                          console.log("endM :", endM);
+
+                                          for(let i = startY; i <= endY; i++) {
+                                            for(let j = startM; j <= (i < endY ? 11 : endM); j++) {
+                                              const months = this.state.months;
+                                              const node = document.createElement("option");
+                                              const nodeText = document.createTextNode(`${months[j]}|${i}`);
+                                              node.appendChild(nodeText);
+                                              document.querySelector("#" + select).appendChild(node);
+                                            }
+                                          }
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      };
+      const date = new Date();
+      please.render.options.from("JAN",1970).to(this.state.months[date.getMonth()],date.getFullYear()).using.select("dateSelect");
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <select name="dateSelect" id="dateSelect">
+              <option disabled readOnly value="none" selected="selected">Select a Date</option>
+          </select>
       </div>
     );
   }
